57259bd6c456703e94e21e096cbbc0a4b8b1708e
trulsofverberg <truls.ofverberg.3883@student.uu.se>
2026-01-13 00:29:56 +0100

    Åttonde Commit i labb5. Bygger vidare MovieFileHandler klassen.

--- src/MovieFileHandler.java
+++ src/MovieFileHandler.java
@@ -1,7 +1,19 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class MovieFileHandler
 {
     public static void saveMovies(Iterable<Movie> movies)
     {
-
+        try (PrintWriter writer = new PrintWriter(new FileWriter(FILE_NAME)))
+        {
+            for (Movie movie:movies)
+            {
+                writer.println(movie.toFileString());
+            }
+        } catch (IOException e)
+        {
+            System.out.println("Error saving file.");
+        }
     }
 }
