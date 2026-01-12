354b0d8acc998dcee4642819f4f7b489e641a08a
trulsofverberg <truls.ofverberg.3883@student.uu.se>
2026-01-13 00:37:28 +0100

    Nionde Commit i labb5. Bygger vidare Movie klassen.

--- src/Movie.java
+++ src/Movie.java
@@ -1,5 +1,8 @@
 public class Movie
 {
+    private String title;
+    private int reviewScore;
+
     public Movie(String title, int reviewScore)
     {
         this.title = title;
@@ -18,7 +21,12 @@ public class Movie
 
     public String toFileString()
     {
-        return title + "" + reviewScore;
+        return title + ";" + reviewScore;
     }
 
+    public static Movie fromFileString(String line)
+    {
+        String[] parts = line.split(";");
+        return new Movie(parts[0], Integer.parseInt(parts[1]));
+    }
 }
