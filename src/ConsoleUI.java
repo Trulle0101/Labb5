e851f8a2412183c1453b8da3cde9bb58064a61a9
trulsofverberg <truls.ofverberg.3883@student.uu.se>
2026-01-13 12:26:43 +0100

    Tolvte commit i labb5. Bygger vidare på klassen ConsoleUI.

--- src/ConsoleUI.java
+++ src/ConsoleUI.java
@@ -1,8 +1,35 @@
+import java.util.List;
+import java.util.Scanner;
+
 public class ConsoleUI
 {
     public ConsoleUI(MovieDatabase database)
     {
         this.database = database;
         this.scanner = new Scanner(System.in);
+    }
+
+    public void start()
+    {
+        boolean running = true;
+
+        while (running)
+        {
+            printMenu();
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice)
+            {
+                case 1 -> searchTitle();
+                case 2 -> searchScore();
+                case 3 -> addMovie();
+                case 4 ->
+                {
+                    MovieFileHandler.saveMovies(database.getAllMovies());
+                    running = false;
+                }
+            }
+        }
     }
 }
