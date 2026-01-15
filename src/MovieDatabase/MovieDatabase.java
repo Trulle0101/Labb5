8f53eaa05a40def6f0ac2348f80e0b73ccfe4b3e
trulsofverberg <truls.ofverberg.3883@student.uu.se>
2026-01-15 23:52:56 +0100

    Tjugotredje commit i labb5. Gjort färdigt MovieDatabase klassen har kommenterat JavaDoc

--- src/MovieDatabase/MovieDatabase.java
+++ src/MovieDatabase/MovieDatabase.java
@@ -4,16 +4,28 @@ import Movie.Movie;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Ansvarar för att lagra och söka filmer i filmdatabasen
+ */
 public class MovieDatabase
 {
 
     private List<Movie> movies = new ArrayList<>();
 
+    /**
+     * Lägger till en film i databasen
+     * @param movie filmen som ska läggas till
+     */
     public void addMovie(Movie movie)
     {
         movies.add(movie);
     }
 
+    /**
+     * Söker filmer baserat på deras titel
+     * @param keyword sökord
+     * @return lista med matchande filmer
+     */
     public List<Movie> searchByTitle(String keyword)
     {
         List<Movie> result = new ArrayList<>();
@@ -27,6 +39,11 @@ public class MovieDatabase
         return result;
     }
 
+    /**
+     * Söker filmer med ett visst minsta betyg
+     * @param minScore lägsta betyg
+     * @return lista med matchande filmer
+     */
     public List<Movie> searchByScore(int minScore)
     {
         List<Movie> result = new ArrayList<>();
@@ -40,6 +57,9 @@ public class MovieDatabase
         return result;
     }
 
+    /**
+     * @return returnerar alla filmer i databasen
+     */
     public List<Movie> getAllMovies()
     {
         return movies;
