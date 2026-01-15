ffa271e2e5e5fc81b1444dcbef8612244244021c
trulsofverberg <truls.ofverberg.3883@student.uu.se>
2026-01-15 22:27:23 +0100

    Artonde commit i labb5. Kompilerat klart Main klassen, programmet ska kunna köras.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import ConsoleUI.ConsoleUI;
+import MovieDatabase.MovieDatabase;
+import MovieFileHandler.MovieFileHandler;
+
 public class Main
 {
     public static void main(String[] args)
     {
-
+        MovieDatabase database = new MovieDatabase();
+        MovieFileHandler.loadMovies(database);
+        ConsoleUI ui = new ConsoleUI(database);
+        ui.start();
     }
 }
