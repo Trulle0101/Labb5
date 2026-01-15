89c2c78d4a3522c1ed292d2fa9c7e4af88b4c4c6
trulsofverberg <truls.ofverberg.3883@student.uu.se>
2026-01-15 22:37:57 +0100

    Nittonde commit i labb5. Ändrade MovieFileHandler klassen, det saknade java.io-imports, fick lägga till det.

--- src/MovieFileHandler/MovieFileHandler.java
+++ src/MovieFileHandler/MovieFileHandler.java
@@ -3,6 +3,8 @@ import MovieDatabase.MovieDatabase;
 import Movie.Movie;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
