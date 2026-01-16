d0fc67eadf8efe323a6a3113fe9ce10de93c1b04
trulsofverberg <truls.ofverberg.3883@student.uu.se>
2026-01-16 12:46:43 +0100

    Tjugoskette commit i labb5. Gjort färdigt Main klassen har kommenterat JavaDoc

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,17 @@ import ConsoleUI.ConsoleUI;
 import MovieDatabase.MovieDatabase;
 import MovieFileHandler.MovieFileHandler;
 
+/**
+ * Start klass för filmdatabasprogrammet.
+ * Ansvarar för att starta programmet och koppla ihop programmets olika klasser
+ */
 public class Main
 {
+    /**
+     * Programemt startpunkt.
+     * Skapar databasen, laddar filmer från fil och startar användargränssnittet.
+     * @param args
+     */
     public static void main(String[] args)
     {
         MovieDatabase database = new MovieDatabase();
